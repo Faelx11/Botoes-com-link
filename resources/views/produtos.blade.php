3aeb9c12cfa8a569d8956c9f909741419f97a235
Faelx11 <rafael.silva2103@etec.sp.gov.br>
2023-02-24 18:04:48 -0300

    Botões com rotas

---
+++ resources/views/produtos.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Botões com link</title>
+        <p style= "color : blue">PRODUTOS</p>
+        
+        <div>
+        <a href="contato"><button><b>contato</b></button></a><img src="img/contato.png" heigth="25" width="25" hspace="10">
+        </div>
+        <br> 
+        <div>
+        <a href="empresa"><button><b>empresa</b></button></a><img src="img/empresa.png" heigth="25" width="25" hspace="10">
+        </div>
+        <br> 
+        <div>
+        <a href="produtos"><button><b>produtos</b></button></a><img src="img/produtos.png" heigth="25" width="25" hspace="10">
+        </div>
+        <br> 
+        <div>
+        <a href="home"><button><b>home</b></button></a><img src="img/home.png" heigth="25" width="25" hspace="10">
+        </div>
+        <br> 
+    </head>
+    <body class="antialiased">
+        
+    </body>
+</html>
